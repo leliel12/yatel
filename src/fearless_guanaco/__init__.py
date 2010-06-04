2feeb1dd9ca7293567d5ed37841cf4dd60a071b7
juan <juan@JuanInfoxel>
2010-06-04 13:27:46 -0300

    migrating from fg

---
+++ src/fearless_guanaco/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# "__init__.py" is part of "Fearless Guanaco"
+#                                    (http://code.google.com/p/fearlessguanaco/)
+# Copyright (C) 2010 UTN-KDD Group <UTN_KDD@googlegroups.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+################################################################################
+# DOCS
+################################################################################
+
+"""Haplotypes Net Analizator"""
+
+
+################################################################################
+# META
+################################################################################
+
+__prj__ = "Fearless Guanaco"
+__version__ = "0.1"
+__license__ = "GPL3"
+__author__ = "JBC"
+__since__ = "0.1"
+__date__ = "2010/04/19"
+
+
+################################################################################
+# MAIN
+################################################################################
+
+if __name__ == "__main__":
+    print __doc__
