492aeb56a511b78419d9f038f6c79b2904d37ba0
juan.bc <juan.bc@localhost>
2010-11-04 20:40:24 -0300

    add an analisis db support

--- src/yatel/NetworkAnalisisDB.py
+++ src/yatel/NetworkAnalisisDB.py
@@ -50,48 +50,136 @@ __date__ = "2010-09-14"
 # IMPORTS
 ################################################################################
 
-from elixir import *
+import elixir
+import string
 
 import Network
 
+#===============================================================================
+# DB CONNECT CLASS
+#===============================================================================
+
+class DBTemplate(object):
+    
+    def __init__(self, template, default_host, default_port):
+        self.template = string.Template(template)
+        self.default_host = default_host
+        self.default_port = default_port
+
+    def substitute(self, db_name, user=None, password=None, host=None, port=None):
+        user = user if user else ""
+        password = password if password else ""
+        host = host if host else self.default_host
+        port = port if port else self.default_port
+        return self.template.substitute(db_name=db_name, 
+                                        user=user, password=password,
+                                        host=host, port=port)
+
 
 ################################################################################
-# ANALISIS DB
+# 
 ################################################################################
 
-class Haplotype(Entity):
-    id = Field(Unicode(1024), primary_key=True)
-    attributes = OneToMany("HaplotypeAtt")
-    facts = OneToMany("Fact")
+class Haplotype(elixir.Entity):
+    
+    id = elixir.Field(elixir.Unicode(1024), primary_key=True)
+    attributes = elixir.OneToMany("HaplotypeAtt")
+    facts = elixir.OneToMany("Fact")
+    
+    elixir.using_options(tablename="haplotypes") 
 
 
-class HaplotypeAtt(Entity):
-    value = Field(UnicodeText())
-    haplotype = ManyToOne("Haplotype")
+#===============================================================================
+# 
+#===============================================================================
+
+class HaplotypeAtt(elixir.Entity):
+    
+    value = elixir.Field(elixir.UnicodeText())
+    haplotype = elixir.ManyToOne("Haplotype")
+    
+    elixir.using_options(tablename="haplotypes_attributes")
+    
+    
+#===============================================================================
+# 
+#===============================================================================
+
+class Fact(elixir.Entity):
+    
+    haplotype = elixir.ManyToOne("Haplotype")
+    attributes = elixir.OneToMany("FactAtt")
+    
+    elixir.using_options(tablename="facts")
 
 
-class Fact(Entity):
-    haplotype = ManyToOne("Haplotype")
-    attributes = OneToMany("FactAtt")
+#===============================================================================
+# 
+#===============================================================================
+
+class FactAtt(elixir.Entity):
+    
+    name = elixir.Field(elixir.UnicodeText())
+    value = elixir.Field(elixir.UnicodeText())
+    fact = elixir.ManyToOne("Fact")
+    
+    elixir.using_options(tablename="fact_attributes")
 
 
-class FactAtt(Entity):
-    name = Field(UnicodeText())
-    value = Field(UnicodeText())
-    fact = ManyToOne("Fact")
+#===============================================================================
+# 
+#===============================================================================
 
-
-class Distance(Entity):
-    hfrom = ManyToOne("Haplotype")
-    hto = ManyToOne("Haplotype")
-    value = Field(Float)
+class Distance(elixir.Entity):
+    
+    hfrom = elixir.ManyToOne("Haplotype")
+    hto = elixir.ManyToOne("Haplotype")
+    value = elixir.Field(elixir.Float)
+    
+    elixir.using_options(tablename="distances")
+    
     
 #===============================================================================
 # FUNCTIONS
 #===============================================================================
 
-def connect(connection, create=False):
-    pass
+ 
+_DBS = {
+    "memory": DBTemplate("sqlite:///:memory:", "", ""),
+    "sqlite": DBTemplate("sqlite:///${db_name}", "", "")
+}
+ 
+ 
+def valid_connections():
+    return _DBS.keys()
+ 
+    
+def connect(db, db_name, 
+            user="", password="", host=None, port="", 
+            create=False, echo=False):
+    elixir.metadata.bind = _DBS[db].substitute(db_name, user, password, host, port)
+    elixir.setup_all(create)
+    elixir.metadata.bind.echo = echo
+    
+
+def commit():
+    elixir.session.commit()
+
+
+def rollback():
+    elixir.session.rollback()
+    
+    
+def execute(query):
+    return elixir.session.execute(query)
+
+
+def close():
+    elixir.session.close()
+
+
+
+    
 
 ################################################################################
 # MAIN
