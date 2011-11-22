fa44ba958cba22405bc7957c111b836e957e75d8
leliel12 <leliel12@localhost>
2011-11-22 15:25:53 -0300

    change simple msg

--- yatel/resources/__init__.py
+++ yatel/resources/__init__.py
@@ -5,6 +5,6 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get(filename):
     filepath = os.path.join(PATH, filename)
-    if not os.path.exists(filepath):
-        raise IOError(2, 'No such file or directory', filename)
+    if not os.path.isfile(filepath):
+        raise IOError(2, 'No such file', filename)
     return filepath
