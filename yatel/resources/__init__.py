f40210533c3c71c0a172bd37b90f065f9f726c96
leliel12 <leliel12@localhost>
2012-05-01 19:01:07 -0300

    add docs to resources

--- yatel/resources/__init__.py
+++ yatel/resources/__init__.py
@@ -1,10 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#===============================================================================
+# DOCS
+#===============================================================================
+
+"""This package contains all the resources (icons, images and sounds) of the
+project. Also has a function to simplify the access to this files bye his 
+filename
+
+"""
+
+#===============================================================================
+# IMPORTS
+#===============================================================================
 
 import os
 
+
+#===============================================================================
+# CONSTANTS
+#===============================================================================
+
 PATH = os.path.dirname(os.path.abspath(__file__))
 
+#===============================================================================
+# FUNCTIONS
+#===============================================================================
+
 def get(filename):
+    """This function retur a full path to a given resource
+    
+    Example:
+    
+        >>> resources.get("filename.ext")
+        "path/to/resources/filename.ext"
+    
+    """
     filepath = os.path.join(PATH, filename)
     if not os.path.isfile(filepath):
         raise IOError(2, 'No such file', filename)
     return filepath
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+
+if __name__ == "__main__":
+    print(__doc__)
