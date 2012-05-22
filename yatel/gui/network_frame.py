57b18d3a096cbf8fa6f47f622589f47f5284eda7
leliel12 <leliel12@localhost>
2012-05-22 17:56:41 -0300

    add x y to nodes

--- yatel/gui/network_frame.py
+++ yatel/gui/network_frame.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     path = os.path.abspath(os.path.dirname(__file__))
     rel = os.path.join(path, "..", "..")
     sys.path.insert(0, rel)
+    
 
 #===============================================================================
 # IMPORTS
@@ -17,24 +18,39 @@ if __name__ == "__main__":
 
 import pilas
 from pilas import actores
+from pilas import imagenes
 from pilas import habilidades
 
 from yatel import dom
+
+from yatel.gui import resources
+
 
 #===============================================================================
 # ACTOR NODO
 #===============================================================================
 
-class HaplotypeActor(actores.Actor, actores.Texto):
+class HaplotypeActor(actores.Texto):
     
-    def __init__(self, hap, x, y):
-        self._hap = hap
+    def __init__(self, hap, x=0, y=0):
+        assert isinstance(hap, dom.Haplotype)
+        super(HaplotypeActor, self).__init__()
+        self.haplotype = hap
         
-        self.texto
+        # conf
+        self.x, self.y = x, y
         self.aprender(habilidades.Arrastrable)
         
+    @property
+    def haplotype(self):
+        return self._hap
         
-
+    @haplotype.setter
+    def haplotype(self, hap):
+        self._hap = hap
+        self.texto = unicode(hap.hap_id)
+        self.imagen = imagenes.cargar_grilla(resources.get("node.svg"), 2)
+        
 
 #===============================================================================
 # MAIN
@@ -43,4 +59,11 @@ class HaplotypeActor(actores.Actor, actores.Texto):
 if __name__ == "__main__":
     motor = pilas._crear_motor("qt")
     pilas.mundo = pilas.Mundo(motor, ancho=800, alto=600, titulo="Yatel")
+    a0=HaplotypeActor(dom.Haplotype("jjj"), x=10)
+    a1=HaplotypeActor(dom.Haplotype("kkk"))
+    
     pilas.ejecutar()
+    
+    
+    print a0, a1
+    
