27e9ee4ef9bfe21709e86f7d763bebc11f2d8c1d
leliel12 <leliel12@localhost>
2012-05-22 00:14:20 -0300

    add skeleton for actors

--- yatel/gui/network_frame.py
+++ yatel/gui/network_frame.py
@@ -1,7 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#===============================================================================
+# TEST PROPUSES PATCH
+#===============================================================================
+
+if __name__ == "__main__":
+    import sys, os
+    path = os.path.abspath(os.path.dirname(__file__))
+    rel = os.path.join(path, "..", "..")
+    sys.path.insert(0, rel)
+
+#===============================================================================
+# IMPORTS
+#===============================================================================
+
 import pilas
+from pilas import actores
+from pilas import habilidades
 
-motor = pilas._crear_motor("qt")
+from yatel import dom
 
-pilas.mundo = pilas.Mundo(motor, ancho=800, alto=600, titulo="test")
+#===============================================================================
+# ACTOR NODO
+#===============================================================================
 
-pilas.ejecutar()
+class HaplotypeActor(actores.Actor, actores.Texto):
+    
+    def __init__(self, hap, x, y):
+        self._hap = hap
+        
+        self.texto
+        self.aprender(habilidades.Arrastrable)
+        
+        
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+
+if __name__ == "__main__":
+    motor = pilas._crear_motor("qt")
+    pilas.mundo = pilas.Mundo(motor, ancho=800, alto=600, titulo="Yatel")
+    pilas.ejecutar()
