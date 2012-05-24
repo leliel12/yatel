5101c98b65ddcfd53c696da14697f84e123a0b32
leliel12 <leliel12@localhost>
2012-05-24 16:57:11 -0300

    added actor of haplotypes

--- yatel/gui/network_frame.py
+++ yatel/gui/network_frame.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     import sys, os
     path = os.path.abspath(os.path.dirname(__file__))
     rel = os.path.join(path, "..", "..")
+    print rel
     sys.path.insert(0, rel)
     
 
@@ -25,20 +26,27 @@ from yatel import dom
 
 from yatel.gui import resources
 
+#===============================================================================
+# CONSTANTS
+#===============================================================================
 
+IMAGE_NODE_NORMAL = resources.get("node_normal.svg")
+"""IMAGE_NODE_FACT = pilas.imagenes.cargar(resources.get("node_fact.svg"))
+IMAGE_NODE_SELECTED = pilas.imagenes.cargar(resources.get("node_select.svg"))
+"""
 #===============================================================================
 # ACTOR NODO
 #===============================================================================
 
 class HaplotypeActor(actores.Texto):
     
-    def __init__(self, hap, x=0, y=0):
+    def __init__(self, hap, x=0, y=0, z=0):
         assert isinstance(hap, dom.Haplotype)
         super(HaplotypeActor, self).__init__()
         self.haplotype = hap
         
         # conf
-        self.x, self.y = x, y
+        self.x, self.y, self.z = x, y, z
         self.aprender(habilidades.Arrastrable)
         
     @property
@@ -49,7 +57,7 @@ class HaplotypeActor(actores.Texto):
     def haplotype(self, hap):
         self._hap = hap
         self.texto = unicode(hap.hap_id)
-        self.imagen = imagenes.cargar_grilla(resources.get("node.svg"), 2)
+        self.imagen = IMAGE_NODE_NORMAL
         
 
 #===============================================================================
@@ -60,7 +68,7 @@ if __name__ == "__main__":
     motor = pilas._crear_motor("qt")
     pilas.mundo = pilas.Mundo(motor, ancho=800, alto=600, titulo="Yatel")
     a0=HaplotypeActor(dom.Haplotype("jjj"), x=10)
-    a1=HaplotypeActor(dom.Haplotype("kkk"))
+    a1=HaplotypeActor(dom.Haplotype("kkk"), z=5)
     
     pilas.ejecutar()
     
