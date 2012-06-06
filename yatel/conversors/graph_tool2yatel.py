392427b917a8149402e83b4c63c47ba1b3c6878e
leliel12 <leliel12@localhost>
2012-06-06 00:36:35 -0300

    added to graph-tool conversor

--- yatel/conversors/graph_tool2yatel.py
+++ yatel/conversors/graph_tool2yatel.py
@@ -30,7 +30,7 @@ instances
 # IMPORTS
 #===============================================================================
 
-import csvcool
+from graph_tool import Graph
 
 from yatel import dom
 
@@ -40,10 +40,40 @@ from yatel import dom
 #===============================================================================
 
 def dump(haps, facts, edges):
-    pass
+    nodebuff = {}
+    ug = Graph(directed=False)
+    
+    hap_prop = ug.new_vertex_property("object")
+    for hap in haps:
+        assert isinstance(hap, dom.Haplotype)
+        vertex = ug.add_vertex()
+        hap_prop[vertex] = hap
+        nodebuff[hap.hap_id] = vertex
+    ug.vertex_properties["haplotypes"] = hap_prop
+    
+    fact_prop = ug.new_vertex_property("object")
+    for fact in facts:
+        assert isinstance(fact, dom.Fact)
+        fact_prop[nodebuff[fact.hap_id]] = fact
+    ug.vertex_properties["facts"] = fact_prop
+    
+    edge_prop = ug.new_vertex_property("object")
+    for edge in edges:
+        assert isinstance(edge, dom.Edge)
+        if len(edge.haps_id) != 2:
+            msg = "grap-tool not support hypergraphs"
+            raise ValueError(msg)
+        nodefrom = nodebuff[edge.haps_id[0]]
+        nodeto = nodebuff[edge.haps_id[1]]
+        ug_edge = ug.add_edge(nodefrom, nodeto)
+        edge_prop[ug_edge] = edge
+    ug.edge_properties["edges"] = edge_prop
+    
+    return ug
+        
     
 
-def load(cool_haps, cool_facts, cool_edges):
+def load(graph):
     pass
 
     
