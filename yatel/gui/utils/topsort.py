3657d953d4ba2af86b051edee55c659962a522ac
leliel12 <leliel12@localhost>
2012-06-08 23:57:18 -0300

    formated topsort

--- yatel/gui/utils/topsort.py
+++ yatel/gui/utils/topsort.py
@@ -1,8 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#===============================================================================
+# IMPORTS
+#===============================================================================
+
+from yatel import dom
 
 
-def to_dict_graph(*edges):
+#===============================================================================
+# FUNCTIONS
+#===============================================================================
+
+def to_dict_graph(edges):
+    """Convert an iterable of edges in a dict with ``keys`` equals
+    ``edge.haps_id[0]``, and ``value`` equals ``edge.haps_id[1:].
+    The nodes without childs has values with empty list.
+
+    **Example**
+
+    ::
+        >>> from yatel.dom import Edge
+        >>> edges = (
+            Edge(1, "id0", "id1"), Edge(1, "id2", "id3"), Edge(1, "id4", "id1")
+        )
+        >>> to_dict_graph(edges)
+        {
+            'id4': ['id1'], 'id2': ['id3'],
+            'id3': ['id3'], 'id0': ['id1'], 'id1': ['id1']
+        }
+
+
+
+
+    """
     gd = {}
     for e in edges:
+        assert isinstance(e, dom.Edge)
         stk = list(e.haps_id)
         key = stk.pop(0)
         if key not in gd:
@@ -15,20 +49,20 @@ def to_dict_graph(*edges):
     return gd
 
 
-def strongly_connected_components(*edges):
+def strongly_connected_components(edges):
     """ Find the strongly connected components in a graph using
     Tarjan's algorithm.
 
     Graph should be a dictionary mapping node names to
     lists of successor nodes.
-    
-    by Paul Harrison 
+
+    by Paul Harrison
     http://www.logarithmic.net/pfh-files/blog/01208083168/sort.py
-    
+
     """
-    
-    graph = to_dict_graph(*edges)
-    
+
+    graph = to_dict_graph(edges)
+
     result = [ ]
     stack = [ ]
     low = { }
@@ -58,15 +92,15 @@ def strongly_connected_components(*edges):
     return result
 
 
-def robust_topological_sort(*edges):
+def sort(edges):
     """First identify strongly connected components,
     then perform a topological sort on these components.
-    
-    by Paul Harrison 
+
+    by Paul Harrison
     http://www.logarithmic.net/pfh-files/blog/01208083168/sort.py
-    
+
     """
-    
+
     def topological_sort(graph):
         count = { }
         for node in graph:
@@ -85,7 +119,7 @@ def robust_topological_sort(*edges):
                     ready.append(successor)
         return result
 
-    components = strongly_connected_components(*edges)
+    components = strongly_connected_components(edges)
 
     node_component = { }
     for component in components:
@@ -96,7 +130,7 @@ def robust_topological_sort(*edges):
     for component in components:
         component_graph[component] = [ ]
 
-    graph = to_dict_graph(*edges)
+    graph = to_dict_graph(edges)
     for node in graph:
         node_c = node_component[node]
         for successor in graph[node]:
@@ -107,27 +141,42 @@ def robust_topological_sort(*edges):
     return topological_sort(component_graph)
 
 
-class Edge(object):
-    def __init__(self, *haps_id):
-        self.haps_id = haps_id
+def xysort(edges, xy_start=(-100, 100), xy_stop=(100, -100), step=(20, 20)):
+    """Topological sort of iterable of edges
 
-if __name__ == '__main__':
-    
-    edges = [
-        Edge(4, 0),
-        Edge(0, 1),
-        Edge(1, 2),
-        Edge(2, 1),
-        Edge(2, 3),
-        Edge(5, 6),
-        
-       
-    ]   
+    **Parameters**
+        :edges:
+            An iterable of edges.
+        :xy_start:
+            Top left corner of the shape.
+        :xy_stop:
+            Bottom right corner of the shape.
+        :step:
+            Separation betwen nodes centre in axis.
 
-    
-    print robust_topological_sort(
-        
-        *edges
-    )
+    """
+
+    xysorted = {}
+    xstep, ystep = step
+    xtop, ytop = xy_stop
+
+    y = xy_start[1]
+    for row in sort(edges):
+        x = xy_start[0]
+        for c in row:
+            xysorted[c] = (x, y)
+            if x + xstep < xtop:
+                x += xstep
+            elif y - ystep > ytop:
+                y -= y
+        if y - ystep > ytop:
+            y -= ystep
+    return xysorted
 
 
+#===============================================================================
+# MAIN
+#===============================================================================
+
+if __name__ == "__main__":
+    print(__doc__)
