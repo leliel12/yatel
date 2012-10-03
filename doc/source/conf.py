69fea589133eaf8cfb9e3664189a38fb6ee93758
leliel12 <leliel12@localhost>
2012-10-03 00:44:47 -0300

    upload something

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -44,7 +44,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
             else:
                 return Mock()
 
-    MOCK_MODULES = "pilas graph_tool"
+    MOCK_MODULES = "pilas graph_tool pilas PyQt4"
     for mod_name in MOCK_MODULES.split():
         sys.modules[mod_name] = Mock()
 
