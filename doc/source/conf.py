8d864c7f0bb906e5aff680810872964766c679c8
leliel12 <leliel12@localhost>
2012-10-03 00:42:04 -0300

    upload something

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,6 +24,31 @@ sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 # READ THE DOCS!
 #===============================================================================
 
+if os.environ.get('READTHEDOCS', None) == 'True':
+
+    class Mock(object):
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __call__(self, *args, **kwargs):
+            return Mock()
+
+        @classmethod
+        def __getattr__(cls, name):
+            if name in ('__file__', '__path__'):
+                return '/dev/null'
+            elif name[0] == name[0].upper():
+                mockType = type(name, (), {})
+                mockType.__module__ = __name__
+                return mockType
+            else:
+                return Mock()
+
+    MOCK_MODULES = "pilas graph_tool"
+    for mod_name in MOCK_MODULES.split():
+        sys.modules[mod_name] = Mock()
+
+
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
