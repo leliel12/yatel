4747062ff1573b74ec90bacc69f52ab87c7f0cf7
leliel12 <leliel12@localhost>
2012-10-03 00:08:16 -0300

    added build of html

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,9 +25,6 @@ sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
 #===============================================================================
 
 if os.environ.get('READTHEDOCS', None) == 'True':
-    import shutil
-    print "JEJEJE 2"
-    print os.path.abspath(__file__)
     sys.exit(0)
 
 
