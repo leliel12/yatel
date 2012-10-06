cccf7d8c9e1908adce921eb2bac8ecb3a773fd15
leliel12 <leliel12@localhost>
2012-10-05 21:18:36 -0300

    test of hugo solution

--- doc/source/mocks.py
+++ doc/source/mocks.py
@@ -9,6 +9,8 @@
 import sys
 
 
+NOT_CLASS = ("EDir", )
+
 class Mock(object):
     def __init__(self, *args, **kwargs):
             pass
@@ -19,13 +21,13 @@ class Mock(object):
     def __getattribute__(cls, name):
         if name in ('__file__', '__path__'):
             return '/dev/null'
-        elif name[0] == name[0].upper():
+        elif name[0] == name[0].upper() and name not in NOT_CLASS:
             mockType = type(name, (), {})
             mockType.__module__ = __name__
             return mockType
         else:
             return Mock()
 
-MOCK_MODULES = ("graph_tool", "PyQt4", "numpy",  "QtCore2", "QtGui", "sip")
+MOCK_MODULES = ("graph_tool", "PyQt4", "numpy",  "QtCore2", "QtGui", "sip", "pycante")
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
