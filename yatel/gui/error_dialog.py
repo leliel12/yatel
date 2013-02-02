51bf56c8c3add92c33643289fc59ade0f44170ec
Juan BC <jbc.develop@gmail.com>
2013-02-02 18:36:28 -0300

    fixed a potential bug of creation of error dialog

--- yatel/gui/error_dialog.py
+++ yatel/gui/error_dialog.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # "THE WISKEY-WARE LICENSE":
-# <utn_kdd@googlegroups.com> wrote this file. As long as you retain this notice 
+# <utn_kdd@googlegroups.com> wrote this file. As long as you retain this notice
 # you can do whatever you want with this stuff. If we meet some day, and you
 # think this stuff is worth it, you can buy us a WISKEY us return.
 
@@ -35,28 +35,28 @@ from yatel.gui import uis
 
 class ErrorDialog(uis.UI("ErrorDialog.ui")):
     """A simple implementation for create dialog using an exception
-    
+
     """
-    
+
     def __init__(self, title, msg, stack, parent=None):
         """Create a new instance of ``ErrorDialog``
-        
+
         **Params**
             :title: A text for use as title of the dialog.
             :msg: A description of the error.
             :stack: Stack trace of the error as string.
             :parent: A gui parent of this widget.
-        
+
         """
         super(ErrorDialog, self).__init__(parent=parent)
         self.stack = stack
         self.setWindowTitle(title)
-        self.messageLabel.setText(msg)
-    
+        self.messageLabel.setText(unicode(msg))
+
     @QtCore.pyqtSlot()
     def on_detailsPushButton_clicked(self):
         """Slot executed when ``detailsPushButton`` is clicked.
-        
+
         This methos show a text of the stacktrace as a widget.
 
         """
@@ -64,25 +64,25 @@ class ErrorDialog(uis.UI("ErrorDialog.ui")):
         self.stackTextBrowser.setText(self.stack)
         self.verticalLayout.addWidget(self.stackTextBrowser)
 
-    
+
 #===============================================================================
 # FUNCTIONS
 #===============================================================================
 
 def critical(title, err):
     """Shorcut for show a modal dialog of a critical error
-    
+
     **Params**
         :title: A title of the dialog.
         :err: An exception istance for extract message and stack texts
-        
+
     """
     msg = getattr(err, "msg", None) \
         or getattr(err, "message", None)\
         or str(err)
     stack = "\n".join(traceback.format_exception(*sys.exc_info()))
     ErrorDialog(title, msg, stack).exec_()
-    
+
 
 #===============================================================================
 # MAIN
