daea423b5a8432e879d0fde24400608fb2d46a38
Juan BC <jbc.develop@gmail.com>
2013-07-19 11:06:48 -0300

    added sqlalchemy implementation of db

---
+++ benchmark.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# "THE WISKEY-WARE LICENSE":
+# <jbc.develop@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a WISKEY in return Juan BC
+
+#===============================================================================
+# IMPORTS
+#===============================================================================
+
+import os, random, collections, hashlib, time, pprint
+
+from yatel import db, db2, dom
+
+import numpy as np
+
+#===============================================================================
+# PRE CONFIG
+#===============================================================================
+
+path = os.path.dirname(os.path.abspath(__file__))
+peewee_path = os.path.join(path, "data", "bench_pewee.db")
+dal_path = os.path.join(path, "data", "bench_dal.db")
+
+if os.path.exists(peewee_path):
+    os.remove(peewee_path)
+if os.path.exists(dal_path):
+    os.remove(dal_path)
+
+
+#===============================================================================
+# BENCH DEC
+#===============================================================================
+
+benchs = collections.OrderedDict()
+funcs = []
+
+def bench(times=1):
+    def _bench(func):
+        funcs.append((func, times))
+        return func
+    return _bench
+
+
+def run():
+    for func, times in funcs:
+        results = []
+        for _ in range(times):
+            start = time.time()
+            func()
+            end = time.time()
+            results.append(end-start)
+        benchs[func.__name__] = np.average(results)
+
+
+#===============================================================================
+# CREATE SOME NETWORKS
+#===============================================================================
+
+def create_network():
+    choices = {
+        int: lambda: random.randint(1, 100),
+        float: lambda: random.randint(1, 100) + random.random(),
+        str: lambda: hashlib.sha1(str(random.random)).hexdigest(),
+        bool: lambda: bool(random.randint(0, 1)),
+    }
+
+    haps = []
+    for idx in range(random.randint(10, 50)):
+        name = "haplotype_" + str(idx)
+        attrs = {}
+        for jdx, func in enumerate(sorted(choices.values())):
+            attrs["attr_" + str(jdx)] = func()
+        haps.append(dom.Haplotype(name, **attrs))
+
+    facts = []
+    for _ in range(random.randint(10, 50)):
+        hap_id = random.choice(haps).hap_id
+        attrs = {}
+        for jdx, func in enumerate(sorted(choices.values())):
+            attrs["attr_" + str(jdx)] = func()
+        facts.append(dom.Fact(hap_id, b=1, c=2, k=2))
+
+    edges = []
+    for _ in range(random.randint(10, 50)):
+        weight = choices[float]()
+        hap_0 = random.choice(haps).hap_id
+        hap_1 = random.choice(haps).hap_id
+        while hap_1 == hap_0:
+            hap_1 = random.choice(haps).hap_id
+        edges.append(dom.Edge(weight, hap_0, hap_1))
+
+    return (haps, facts, edges)
+
+#===============================================================================
+# THE NETWORK
+#===============================================================================
+
+haps, facts, edges = create_network()
+
+#===============================================================================
+# CREATE THE two db
+#===============================================================================
+
+peewee_db = None
+dal_db = None
+
+@bench()
+def create_peewee():
+    conn = db.YatelConnection("sqlite", name=peewee_path)
+    conn.init_with_values(haps, facts, edges)
+
+@bench()
+def create_dal():
+    conn = db2.YatelNetwork("sqlite", dbname=dal_path, create=True)
+    map(conn.add_element, haps)
+    map(conn.add_element, facts)
+    map(conn.add_element, edges)
+    conn.end_creation()
+
+@bench()
+def connect_peewee():
+    global peewee_db
+    peewee_db = db.YatelConnection("sqlite", name=peewee_path)
+    peewee_db.init_yatel_database()
+
+@bench()
+def connect_dal():
+    global dal_db
+    dal_db = db2.YatelNetwork("sqlite", dbname=dal_path)
+
+@bench(100)
+def iter_haplotypes_peewee():
+    list(peewee_db.iter_haplotypes())
+
+@bench(100)
+def iter_haplotypes_dal():
+    list(dal_db.iter_haplotypes())
+
+@bench(100)
+def iter_facts_peewee():
+    list(peewee_db.iter_facts())
+
+@bench(100)
+def iter_facts_dal():
+    list(dal_db.iter_facts())
+
+@bench(100)
+def iter_edges_peewee():
+    list(peewee_db.iter_edges())
+
+@bench(100)
+def iter_edges_dal():
+    list(dal_db.iter_edges())
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+
+if __name__ == "__main__":
+    run()
+    for k, v in benchs.items():
+        print "{} -> {}".format(k, v)
---
+++ yatel/db3.py
@@ -0,0 +1,379 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# "THE WISKEY-WARE LICENSE":
+# <utn_kdd@googlegroups.com> wrote this file. As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy us a WISKEY us return.
+
+
+#===============================================================================
+# DOCS
+#===============================================================================
+
+"""Database abstraction layer
+
+This new backend has almost the same method than the original db.py and in the
+future will be the only one.
+
+Is developed over sqlalchemy
+
+for testing use:
+
+::
+
+    from yatel import db3 as db
+
+"""
+
+
+#===============================================================================
+# IMPORTS
+#===============================================================================
+
+import datetime
+import string
+import decimal
+
+from yatel import dom
+
+import sqlalchemy as sql
+
+
+#===============================================================================
+# EXCEPTIONS
+#===============================================================================
+
+# The order to show schema variables, if it not in this list put at the end
+VARS_ORDER = ("dbname", "user", "password", "dsn", "host", "port")
+
+SCHEMAS = (
+    "sqlite",
+    #"mysql",
+    #"postgres",
+
+)
+
+
+SCHEMA_URIS = {
+    "sqlite": 'sqlite:///${dbname}',
+}
+
+
+SCHEMA_VARS = {}
+for schema in SCHEMAS:
+    tpl = string.Template(SCHEMA_URIS[schema])
+    variables = []
+    for e, n, b, i in tpl.pattern.findall(tpl.template):
+        if n or b:
+            variables.append(n or b)
+    variables.sort(key=lambda v: VARS_ORDER.index(v)
+                                 if v in VARS_ORDER
+                                 else len(VARS_ORDER))
+    SCHEMA_VARS[schema] = variables
+
+
+DAL_TYPES = {
+    datetime.datetime:
+        lambda x: sql.DateTime(),
+    datetime.time:
+        lambda x: sql.Time(),
+    datetime.date:
+        lambda x: sql.Date(),
+    bool:
+        lambda x: sql.Boolean(),
+    int:
+        lambda x: sql.Integer(),
+    float:
+        lambda x: sql.Float(),
+    str:
+        lambda x: sql.String(512) if len(x) < 512 else sql.Text(),
+    unicode:
+        lambda x: sql.String(512) if len(x) < 512 else sql.Text(),
+    decimal.Decimal:
+        lambda x: sql.Numeric()
+}
+
+FK = "FK"
+
+
+#===============================================================================
+# ERROR
+#===============================================================================
+
+class YatelNetworkError(Exception):
+    """Error for use when some *Yatel* logic fail in database."""
+    pass
+
+
+#===============================================================================
+# NETWORK
+#===============================================================================
+
+
+class YatelNetwork(object):
+
+    def __init__(self, schema, create=False, **kwargs):
+        tpl = string.Template(SCHEMA_URIS[schema])
+        self._uri = tpl.substitute(kwargs)
+        self._metadata = sql.MetaData(self._uri)
+        self._hapid_buff = {}
+        self._dbid_buff = {}
+        self._create_mode = create
+
+        if self._create_mode:
+            self._create_network_base_tables()
+        else:
+            self._metadata.reflect()
+
+    #===========================================================================
+    # PRIVATE
+    #===========================================================================
+
+    def _create_network_base_tables(self):
+
+        # first add the static tables
+        self._yatel_fields = sql.Table(
+            'yatel_fields', self._metadata,
+            sql.Column("id", sql.Integer(), primary_key=True),
+            sql.Column("tname", sql.String(20), nullable=False),
+            sql.Column("fname", sql.String(512), nullable=False),
+            sql.Column("ftype", sql.String(20), nullable=False),
+            sql.Column("tname", sql.Boolean(), nullable=False),
+            sql.Column("reference_to", sql.String(20), nullable=True)
+        )
+
+        self._yatel_versions = sql.Table(
+            'yatel_versions', self._metadata,
+            sql.Column("id", sql.Integer(), primary_key=True),
+            sql.Column("tag", sql.String(512), unique=True,nullable=False),
+            sql.Column("datetime", sql.DateTime(), nullable=False),
+            sql.Column("comment", sql.Text(), nullable=False),
+            sql.Column("data", sql.PickleType(), nullable=False),
+        )
+
+        # base struct
+        self._haplotypes = sql.Table(
+            'haplotypes', self._metadata,
+            sql.Column("id", sql.Integer(), primary_key=True),
+            sql.Column("hap_id", sql.String(512), unique=True, nullable=False)
+        )
+
+        self._facts = sql.Table(
+            'facts', self._metadata,
+            sql.Column("id", sql.Integer(), primary_key=True),
+            sql.Column("hap", sql.Integer(), sql.ForeignKey('haplotypes.id')),
+        )
+
+        self._facts = sql.Table(
+            'edges', self._metadata,
+            sql.Column("id", sql.Integer(), primary_key=True),
+            sql.Column("weight", sql.Float(), nullable=False),
+        )
+
+        self._metadata.create_all()
+
+    #~ def _hapid2dbid(self, hap_id):
+        #~ if hap_id not in self._hapid_buff:
+            #~ query = self._dal.haplotypes.hap_id == hap_id
+            #~ row = self._dal(query).select(self._dal.haplotypes.id).first()
+            #~ self._hapid_buff[hap_id] = row["id"]
+        #~ return self._hapid_buff[hap_id]
+#~
+    #~ def _dbid2hapid(self, db_id):
+        #~ if db_id not in self._dbid_buff:
+            #~ query = self._dal.haplotypes.id == db_id
+            #~ row = self._dal(query).select(self._dal.haplotypes.hap_id).first()
+            #~ self._dbid_buff[db_id] = row["hap_id"]
+        #~ return self._dbid_buff[db_id]
+#~
+    #~ def _new_attrs(self, attnames, table):
+        #~ return set(attnames).difference(table.fields)
+#~
+    #~ def _row2hap(self, row):
+        #~ attrs = dict([
+            #~ (k, v) for k, v in row.as_dict().items()
+            #~ if k not in ("id", "hap_id") and v!= None
+        #~ ])
+        #~ hap_id = row["hap_id"]
+        #~ return dom.Haplotype(hap_id, **attrs)
+#~
+    #~ def _row2fact(self, row):
+        #~ attrs = dict([
+            #~ (k, v) for k, v in row.as_dict().items()
+            #~ if k not in ("id", "hap") and v!= None
+        #~ ])
+        #~ hap_id = self._dbid2hapid(row["hap"])
+        #~ return dom.Fact(hap_id, **attrs)
+#~
+    #~ def _row2edge(self, row):
+        #~ haps = [self._dbid2hapid(v)
+                #~ for k, v in row.as_dict().items()
+                #~ if k not in ("id", "weight") and v!= None]
+        #~ weight = row["weight"]
+        #~ return dom.Edge(weight, *haps)
+#~
+    #~ #===========================================================================
+    #~ # CREATE METHODS
+    #~ #===========================================================================
+#~
+    #~ def add_element(self, elem):
+        #~ if self.created:
+            #~ raise YatelNetworkError("Network already created")
+#~
+        #~ # if is an haplotypes
+        #~ if isinstance(elem, dom.Haplotype):
+            #~ new_attrs_names = self._new_attrs(elem.names_attrs(),
+                                              #~ self._dal.haplotypes)
+            #~ new_attrs = []
+            #~ attrs_descs = []
+            #~ for fname in new_attrs_names:
+                #~ ftype =  DAL_TYPES[type(elem[fname])](elem[fname])
+                #~ field = dal.Field(fname, ftype, notnull=False)
+                #~ desc = {"tname": 'haplotypes', "fname": fname, "ftype": ftype,
+                        #~ "is_unique": False, "reference_to": None}
+                #~ new_attrs.append(field)
+                #~ attrs_descs.append(desc)
+            #~ if new_attrs:
+                #~ self._dal.define_table(
+                    #~ 'haplotypes',
+                    #~ self._dal.haplotypes,
+                    #~ redefine=True, *new_attrs
+                #~ )
+                #~ self._dal.yatel_fields.bulk_insert(attrs_descs)
+#~
+            #~ attrs = dict(elem.items_attrs())
+            #~ attrs.update(hap_id=elem.hap_id)
+            #~ self._dal.haplotypes.insert(**attrs)
+#~
+        #~ # if is a fact
+        #~ elif isinstance(elem, dom.Fact):
+            #~ new_attrs_names = self._new_attrs(elem.names_attrs(),
+                                              #~ self._dal.facts)
+            #~ new_attrs = []
+            #~ attrs_descs = []
+            #~ for fname in new_attrs_names:
+                #~ ftype =  DAL_TYPES[type(elem[fname])](elem[fname])
+                #~ field = dal.Field(fname, ftype, notnull=False)
+                #~ desc = {"tname": 'facts', "fname": fname, "ftype": ftype,
+                        #~ "is_unique": False, "reference_to": None}
+                #~ new_attrs.append(field)
+                #~ attrs_descs.append(desc)
+            #~ if new_attrs:
+                #~ self._dal.define_table(
+                    #~ 'facts',
+                    #~ self._dal.facts,
+                    #~ redefine=True, *new_attrs
+                #~ )
+                #~ self._dal.yatel_fields.bulk_insert(attrs_descs)
+#~
+            #~ attrs = dict(elem.items_attrs())
+            #~ attrs.update(hap=self._hapid2dbid(elem.hap_id))
+            #~ self._dal.facts.insert(**attrs)
+#~
+        #~ # if is an edge
+        #~ elif isinstance(elem, dom.Edge):
+            #~ actual_haps_number = len(self._dal.edges.fields) - 2
+            #~ need_haps_number = len(elem.haps_id)
+#~
+            #~ new_attrs = []
+            #~ attrs_descs = []
+            #~ while need_haps_number > actual_haps_number + len(new_attrs):
+                #~ fname = "hap_{}".format(actual_haps_number + len(new_attrs))
+                #~ field = dal.Field(fname, self._dal.haplotypes, notnull=False)
+                #~ desc = {"tname": 'edges', "fname": fname, "ftype": FK,
+                        #~ "is_unique": False, "reference_to": 'haplotypes'}
+                #~ new_attrs.append(field)
+                #~ attrs_descs.append(desc)
+            #~ if new_attrs:
+                #~ self._dal.define_table(
+                    #~ 'edges',
+                    #~ self._dal.edges,
+                    #~ redefine=True, *new_attrs)
+                #~ self._dal.yatel_fields.bulk_insert(attrs_descs)
+#~
+            #~ attrs = {}
+            #~ for idx, hap_id in enumerate(elem.haps_id):
+                #~ attrs["hap_{}".format(idx)] = self._hapid2dbid(hap_id)
+            #~ attrs.update(weight=elem.weight)
+            #~ self._dal.edges.insert(**attrs)
+#~
+        #~ # if is trash
+        #~ else:
+            #~ msg = "Object '{}' is not yatel.dom type".format(str(elem))
+            #~ raise YatelNetworkError(msg)
+
+    #~ def end_creation(self):
+        #~ if self.created:
+            #~ raise YatelNetworkError("Network already created")
+        #~ self._dal.commit()
+        #~ self._create_mode = False
+
+    #===========================================================================
+    # QUERIES # not use self._dal here!!!!
+    #===========================================================================
+
+    #~ def iter_haplotypes(self):
+        #~ """Iterates over all ``dom.Haplotype`` instances store in the database.
+#~
+        #~ """
+        #~ for row in self.dal(self.dal.haplotypes).select():
+            #~ yield self._row2hap(row)
+#~
+    #~ def iter_facts(self):
+        #~ """Iterates over all ``dom.Fact`` instances store in the database."""
+        #~ for row in self.dal(self.dal.facts).select():
+            #~ yield self._row2fact(row)
+#~
+    #~ def iter_edges(self):
+        #~ """Iterates over all ``dom.Edge`` instances store in the database."""
+        #~ for row in self.dal(self.dal.edges).select():
+            #~ yield self._row2edge(row)
+#~
+    #~ def haplotype_by_id(self, hap_id):
+        #~ """Return a ``dom.Haplotype`` instace store in the dabase with the
+        #~ giver ``hap_id``.
+#~
+        #~ **Params**
+            #~ :hap_id: An existing id of the ``haplotypes`` type table.
+#~
+        #~ **Return**
+            #~ ``dom.Haplotype`` instance.
+#~
+        #~ """
+        #~ query = self.dal.haplotypes.id == self._hapid2dbid(hap_id)
+        #~ row = self.dal(query).select(limitby=(0, 1)).first()
+        #~ return self._row2hap(row)
+
+
+    #===========================================================================
+    # PROPERTIES
+    #===========================================================================
+
+    #~ @property
+    #~ def created(self):
+        #~ return not self._create_mode
+
+
+
+
+#===============================================================================
+# FUNCTIONS
+#===============================================================================
+
+#~ def allin(l1, l2):
+    #~ """Returns ``True`` if all elements in ``l1`` is in ``l2``"""
+    #~ for l in l1:
+        #~ if l not in l2:
+            #~ return False
+    #~ return True
+
+
+#===============================================================================
+# MAIN
+#===============================================================================
+
+
+
+if __name__ == "__main__":
+    print(__doc__)
