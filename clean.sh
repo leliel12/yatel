e824d99acb5a37f14fa1ea04bdc0abc0fae903ed
Juan BC <jbc.develop@gmail.com>
2013-08-22 00:26:28 -0300

    removed unused all files

--- clean.sh
+++ clean.sh
@@ -3,4 +3,6 @@ find . -iname '*.pyo' -delete;
 find . -iname '*.db' -delete;
 find . -iname '*.orig' -delete;
 rm -Rf Yatel.egg-info;
+rm -Rf yatel.egg-info;
+rm -Rf Sphinx_PyPI_upload*.egg;
 rm -Rf dist;
