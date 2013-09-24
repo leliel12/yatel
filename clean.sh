05f33635f38f0dc2e881f726bab4d6b02c5d79bc
Juan BC <jbc.develop@gmail.com>
2013-09-24 16:46:07 -0300

    fixed little thing about clean

--- clean.sh
+++ clean.sh
@@ -3,6 +3,5 @@ find . -iname '*.pyo' -delete;
 find . -iname '*.db' -delete;
 find . -iname '*.orig' -delete;
 rm -Rf Yatel.egg-info;
-rm -Rf yatel.egg-info;
 rm -Rf Sphinx_PyPI_upload*.egg;
 rm -Rf dist;
