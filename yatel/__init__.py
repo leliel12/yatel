5799a9256cbf5dc30cff417916e90487a2bfe545
Juan BC <jbc.develop@gmail.com>
2013-09-24 17:09:45 -0300

    fixed some things

--- yatel/__init__.py
+++ yatel/__init__.py
@@ -61,7 +61,7 @@ CLASSIFIERS = (
 )
 
 # : Home Page of yatel
-URL = "http://bitbucket.org/leliel12/yatel"
+URL = "http://bitbucket.org/yatel/yatel"
 
 # : Download for pypi
 DOWNLOAD_URL = "{}/downloads/{}-{}.tar.gz".format(URL, PRJ.lower(),
@@ -86,7 +86,7 @@ LICENSE = "WISKEY-WARE"
 FULL_LICENSE = u""""THE WISKEY-WARE LICENSE":
 <utn_kdd@googlegroups.com> wrote this file. As long as you retain this notice
 you can do whatever you want with this stuff. If we meet some day, and you
-think this stuff is worth it, you can buy us a WISKEY us return.
+think this stuff is worth it, you can buy us a WISKEY in return.
 
 """
 
@@ -94,7 +94,7 @@ think this stuff is worth it, you can buy us a WISKEY us return.
 KEYWORDS = """Yatel user-profile-distance-based networks  multidimensional
 exploration biology database kdd datamining"""
 
-# : The path to the puser home path
+# : The path to the user home path
 HOME_PATH = os.path.expanduser("~")
 
 # : This is a folder where user put his data
