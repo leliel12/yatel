ebacc554ca7f615ce0a375366ef9889efc19d3c4
la_chiky <la_chiky@la_chiky-PC.mshome.net>
2014-06-03 13:38:51 -0300

    End test class Facts

--- yatel/tests/test_dom.py
+++ yatel/tests/test_dom.py
@@ -6,26 +6,24 @@
 # you can do whatever you want with this stuff. If we meet some day, and you
 # think this stuff is worth it, you can buy us a WISKEY in return.
 
-#===============================================================================
+# ===============================================================================
 # DOC
-#===============================================================================
+# ===============================================================================
 
 """yatel.dom module tests"""
 
 
-#===============================================================================
+# ===============================================================================
 # IMPORTS
-#===============================================================================
-
-import random
+# ===============================================================================
 
 from yatel import dom
 from yatel.tests.core import YatelTestCase
 
 
-#===============================================================================
+# ===============================================================================
 # VALIDATE TESTS
-#===============================================================================
+# ===============================================================================
 
 class TestHaplatoype(YatelTestCase):
 
@@ -48,7 +46,7 @@ class TestHaplatoype(YatelTestCase):
     def test_hash(self):
         hap0 = dom.Haplotype(1)
         hap1 = dom.Haplotype(1, arg="foo")
-        self.assertEquals(hash(hap0),hash(hap1))
+        self.assertEquals(hash(hap0), hash(hap1))
 
     def test_is(self):
         hap0 = dom.Haplotype(1)
@@ -71,14 +69,61 @@ class TestHaplatoype(YatelTestCase):
         self.assertEquals(d[hap2], "fee")
         self.assertEquals(len(d), 2)
 
-
     def test_none_values(self):
         hap0 = dom.Haplotype(1, attr=None)
         self.assertRaises(AttributeError, lambda: hap0.attr)
 
-#===============================================================================
+
+class TestFact(YatelTestCase):
+    def test_getattr(self):
+        fact1 = dom.Fact(1, attr0="fac")
+        self.assertEquals("fac", fact1.attr0)
+        self.assertEquals("fac", fact1["attr0"])
+        self.assertEquals(fact1.attr0, fact1["attr0"])
+
+    def test_eq(self):
+        fact0 = dom.Fact(1, attr0="fac")
+        fact1 = dom.Fact(1, attr0="fac")
+        self.assertEquals(fact0, fact1)
+
+    def test_ne(self):
+        fact0 = dom.Fact(1, attr0="fac", attr1="fact")
+        fact1 = dom.Fact(2, attr0="fac", attr1="fact")
+        self.assertNotEqual(fact0, fact1)
+
+    def test_hash(self):
+        fact0 = dom.Fact(1)
+        fact1 = dom.Fact(1)
+        self.assertEquals(hash(fact0), hash(fact1))
+
+    def test_is(self):
+        fact0 = dom.Fact(1)
+        fact1 = dom.Fact(1, attr0="foo")
+        self.assertFalse(fact0 is fact1)
+        self.assertTrue(fact0 is fact0)
+        self.assertTrue(fact1 is fact1)
+
+    def test_in(self):
+        fact0 = dom.Fact(1)
+        fact1 = dom.Fact(1, attr0="foo")
+        fact2 = dom.Fact(2)
+        d = {fact0: "foo"}
+        self.assertIn(fact0, d)
+        self.assertNotIn(fact1, d)
+        self.assertNotIn(fact2, d)
+        d.update({fact1: "faa", fact2: "fee"})
+        self.assertEquals(d[fact0], "foo")
+        self.assertEquals(d[fact1], "faa")
+        self.assertEquals(d[fact2], "fee")
+        self.assertEquals(len(d), 3)
+
+    def test_none_values(self):
+        fact1 = dom.Fact(1, attr0="foo", attr1=None)
+        self.assertRaises(AttributeError, lambda: fact1.attr1)
+
+# ===============================================================================
 # MAIN
-#===============================================================================
+# ===============================================================================
 
 if __name__ == "__main__":
     print(__doc__)
