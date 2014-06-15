7d9e344a315b4fedfe235fa70ed108d396e5d41d
Juan BC <jbc.develop@gmail.com>
2014-06-15 13:02:18 -0300

    parse uri tested

--- yatel/tests/test_db.py
+++ yatel/tests/test_db.py
@@ -30,17 +30,6 @@ from yatel.tests.core import YatelTestCase
 
 class TestDBFunctions(YatelTestCase):
 
-    def test_enviroments(self):
-        desc = self.nw.describe()
-        fact_attrs = desc["fact_attributes"].keys()
-        for size in xrange(0, len(fact_attrs)):
-            filters = set()
-            while len(filters) < size:
-                f = random.choice(fact_attrs)
-                if f != "hap_id":
-                    filters.add(f)
-            list(self.nw.enviroments(list(filters)))
-
     def test_copy(self):
         anw = db.YatelNetwork(engine="memory", mode=db.MODE_WRITE)
         db.copy(self.nw, anw)
@@ -50,6 +39,20 @@ class TestDBFunctions(YatelTestCase):
             anw_values = getattr(anw, method)()
             self.assertSameUnsortedContent(nw_values, anw_values)
         self.assertEquals(self.nw.describe(), anw.describe())
+
+    def test_parse_uri(self):
+        for mode in db.MODES:
+            for log in [True, False]:
+                parsed = db.parse_uri(
+                    "engine://user:password@host:666/db", mode=mode, log=log
+                )
+                self.assertEquals(parsed["engine"], "engine")
+                self.assertEquals(parsed["host"], "host")
+                self.assertEquals(parsed["port"], 666)
+                self.assertEquals(parsed["user"], "user")
+                self.assertEquals(parsed["password"], "password")
+                self.assertEquals(parsed["mode"], mode)
+                self.assertEquals(parsed["log"], log)
 
 
 #==============================================================================
@@ -84,7 +87,16 @@ class YatelNetwork(YatelTestCase):
         self.assertEqual(len(rs), 1)
         self.assertEqual(rs[0], self.edges[2])
 
-
+    def test_enviroments(self):
+        desc = self.nw.describe()
+        fact_attrs = desc["fact_attributes"].keys()
+        for size in xrange(0, len(fact_attrs)):
+            filters = set()
+            while len(filters) < size:
+                f = random.choice(fact_attrs)
+                if f != "hap_id":
+                    filters.add(f)
+            list(self.nw.enviroments(list(filters)))
 
 
 
