34bd27ae38df95f0ef3b295ebdbc52d1b8afc1ed
Juan BC <jbc.develop@gmail.com>
2015-01-03 00:57:27 -0300

    fixed some documentation

--- yatel/nwstats.py
+++ yatel/nwstats.py
@@ -32,7 +32,7 @@ def haplotypesfreq(nw, env=None, **kwargs):
     """Calculates a frequency of an haplotype of a given enviroment.
 
     Every *enviroment* is defined for a set of *facts*, every *fact* has only
-    one *haplotype*. This methods count how many facts has the same hap_id.
+    one *haplotype*. This method count how many facts has the same hap_id.
 
     Parameters
     ----------
@@ -41,13 +41,20 @@ def haplotypesfreq(nw, env=None, **kwargs):
     env : :py:class:`yatel.dom.Enviroment` or dict like
         Environment for filtering.
 
+    Returns
+    -------
+    haps_ids : An array of k haps_ids present on this enviroment
+        A list of haps ids inside the given enviroment.
+    cnt : An array of k counte
+         The cnt[i] has the frequency of haps_ids[i]
+
     """
     facts = (
         nw.facts_by_environment(env=env, **kwargs)
         if (env or kwargs) else nw.facts()
     )
-    haps_ids = facts.attrs(["hap_id"])
-    return np.unique(haps_ids, return_counts=True)
+    haps_ids, cnt = np.unique(facts.attrs(["hap_id"]), return_counts=True)
+    return haps_ids, cnt
 
 
 # =============================================================================
